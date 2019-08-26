d376107bc57009599cebaf4e91b0899e20ba973e
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-08-26 23:45:53 +0600

    title fixed

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Contact us - Brand</title>
+    <title>Contact us</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic">
