68e024806476a4cdd9539039f231adadc8e7e971
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-08-26 23:33:11 +0600

    header added

---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<nav class="navbar fixed-top navbar-light navbar-expand-lg fixed-top" id="mainNav">
+    <div class="container">
+    <a class="navbar-brand" href="index.html">Ashraf Kabir</a><button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="nav navbar-nav ml-auto">
+                <li class="nav-item" role="presentation"><a class="nav-link" href="index.html">Home</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link" href="about.html">About us</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link" href="contact.html">Contact us</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link" href="post.html">Blog Post</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<header class="masthead" style="background-image:url('assets/img/home-bg.jpg');">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-lg-8 mx-auto">
+                <div class="site-heading">
+                    <h1>Kabir's Blog</h1><span class="subheading">A Blog Theme by Start Bootstrap</span></div>
+            </div>
+        </div>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Home - Brand</title>
+    <title>Home - Ashraf Kabir</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic">
@@ -12,30 +12,9 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-light navbar-expand-lg fixed-top" id="mainNav">
-        <div class="container"><a class="navbar-brand" href="index.html">Ashraf Kabir</a><button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
-            <div
-                class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="nav navbar-nav ml-auto">
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="index.html">Home</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="about.html">About us</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="contact.html">Contact us</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="post.html">Blog Post</a></li>
-                </ul>
-        </div>
-        </div>
-    </nav>
-    <header class="masthead" style="background-image:url('assets/img/home-bg.jpg');">
-        <div class="overlay"></div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-10 col-lg-8 mx-auto">
-                    <div class="site-heading">
-                        <h1>Kabir's Blog</h1><span class="subheading">A Blog Theme by Start Bootstrap</span></div>
-                </div>
-            </div>
-        </div>
-    </header>
+
+    <?php include 'includes/header.php'; ?>
+    
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-lg-8">
