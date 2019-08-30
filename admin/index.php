df02695867aef6979221259dd390a303a963e07a
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-08-31 03:58:25 +0600

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -10,7 +10,7 @@ if (strlen($_SESSION['alogin']) == 0) {
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-        <title>Dashboard - Brand</title>
+        <title>Dashboard - Admin</title>
         <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet"
               href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
