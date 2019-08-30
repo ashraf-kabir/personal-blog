9ca595f36b49af3437618d6ea06f2d92f4477640
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-08-30 21:44:10 +0600

    footer included

--- admin/404.php
+++ admin/404.php
@@ -215,11 +215,10 @@
                     </div>
                 </div>
             </div>
-            <footer class="bg-white sticky-footer">
-                <div class="container my-auto">
-                    <div class="text-center my-auto copyright"><span>Copyright © Brand 2019</span></div>
-                </div>
-            </footer>
+
+            <!-- Footer -->
+            <?php include 'includes/footer.php'; ?>
+
         </div>
         <a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a></div>
     <script src="assets/js/jquery.min.js"></script>
---
+++ admin/includes/footer.php
@@ -0,0 +1,5 @@
+<footer class="bg-white sticky-footer">
+    <div class="container my-auto">
+        <div class="text-center my-auto copyright"><span>Copyright © Ashraf Kabir 2019</span></div>
+    </div>
+</footer>
