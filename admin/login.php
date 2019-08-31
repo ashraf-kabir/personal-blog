474a4e72eded32d4abb6d963d3497cff702a22b9
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-08-31 11:20:52 +0600

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -33,7 +33,7 @@ if (isset($_POST['login'])) {
           href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
     <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
 
     <script type="text/javascript">
         $(document).ready(function () {
