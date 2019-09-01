d71e221fb49e2603a98505928e8b66a06c0bb3d6
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-01 18:43:02 +0600

    Update add-post.php

--- admin/add-post.php
+++ admin/add-post.php
@@ -4,6 +4,72 @@ include('includes/config.php');
 if (strlen($_SESSION['alogin']) == 0) {
     header('location: login.php');
 } else {
+    if (isset($_POST['submit'])) {
+        $title = $_POST['title'];
+        $brand = $_POST['brandname'];
+        $vehicleoverview = $_POST['vehicalorcview'];
+        $priceperday = $_POST['priceperday'];
+        $fueltype = $_POST['fueltype'];
+        $modelyear = $_POST['modelyear'];
+        $seatingcapacity = $_POST['seatingcapacity'];
+        $vimage1 = $_FILES["img1"]["name"];
+        $vimage2 = $_FILES["img2"]["name"];
+        $vimage3 = $_FILES["img3"]["name"];
+        $vimage4 = $_FILES["img4"]["name"];
+        $vimage5 = $_FILES["img5"]["name"];
+        $airconditioner = $_POST['airconditioner'];
+        $powerdoorlocks = $_POST['powerdoorlocks'];
+        $antilockbrakingsys = $_POST['antilockbrakingsys'];
+        $brakeassist = $_POST['brakeassist'];
+        $powersteering = $_POST['powersteering'];
+        $driverairbag = $_POST['driverairbag'];
+        $passengerairbag = $_POST['passengerairbag'];
+        $powerwindow = $_POST['powerwindow'];
+        $cdplayer = $_POST['cdplayer'];
+        $centrallocking = $_POST['centrallocking'];
+        $crashcensor = $_POST['crashcensor'];
+        $leatherseats = $_POST['leatherseats'];
+
+        move_uploaded_file($_FILES["img1"]["tmp_name"], "img/vehicleimages/" . $_FILES["img1"]["name"]);
+        move_uploaded_file($_FILES["img2"]["tmp_name"], "img/vehicleimages/" . $_FILES["img2"]["name"]);
+        move_uploaded_file($_FILES["img3"]["tmp_name"], "img/vehicleimages/" . $_FILES["img3"]["name"]);
+        move_uploaded_file($_FILES["img4"]["tmp_name"], "img/vehicleimages/" . $_FILES["img4"]["name"]);
+        move_uploaded_file($_FILES["img5"]["tmp_name"], "img/vehicleimages/" . $_FILES["img5"]["name"]);
+
+        $sql = "INSERT INTO tblvehicles(VehiclesTitle,VehiclesBrand,VehiclesOverview,PricePerDay,FuelType,ModelYear,SeatingCapacity,Vimage1,Vimage2,Vimage3,Vimage4,Vimage5,AirConditioner,PowerDoorLocks,AntiLockBrakingSystem,BrakeAssist,PowerSteering,DriverAirbag,PassengerAirbag,PowerWindows,CDPlayer,CentralLocking,CrashSensor,LeatherSeats) VALUES(:vehicletitle,:brand,:vehicleoverview,:priceperday,:fueltype,:modelyear,:seatingcapacity,:vimage1,:vimage2,:vimage3,:vimage4,:vimage5,:airconditioner,:powerdoorlocks,:antilockbrakingsys,:brakeassist,:powersteering,:driverairbag,:passengerairbag,:powerwindow,:cdplayer,:centrallocking,:crashcensor,:leatherseats)";
+        $query = $dbh->prepare($sql);
+        $query->bindParam(':vehicletitle', $vehicletitle, PDO::PARAM_STR);
+        $query->bindParam(':brand', $brand, PDO::PARAM_STR);
+        $query->bindParam(':vehicleoverview', $vehicleoverview, PDO::PARAM_STR);
+        $query->bindParam(':priceperday', $priceperday, PDO::PARAM_STR);
+        $query->bindParam(':fueltype', $fueltype, PDO::PARAM_STR);
+        $query->bindParam(':modelyear', $modelyear, PDO::PARAM_STR);
+        $query->bindParam(':seatingcapacity', $seatingcapacity, PDO::PARAM_STR);
+        $query->bindParam(':vimage1', $vimage1, PDO::PARAM_STR);
+        $query->bindParam(':vimage2', $vimage2, PDO::PARAM_STR);
+        $query->bindParam(':vimage3', $vimage3, PDO::PARAM_STR);
+        $query->bindParam(':vimage4', $vimage4, PDO::PARAM_STR);
+        $query->bindParam(':vimage5', $vimage5, PDO::PARAM_STR);
+        $query->bindParam(':airconditioner', $airconditioner, PDO::PARAM_STR);
+        $query->bindParam(':powerdoorlocks', $powerdoorlocks, PDO::PARAM_STR);
+        $query->bindParam(':antilockbrakingsys', $antilockbrakingsys, PDO::PARAM_STR);
+        $query->bindParam(':brakeassist', $brakeassist, PDO::PARAM_STR);
+        $query->bindParam(':powersteering', $powersteering, PDO::PARAM_STR);
+        $query->bindParam(':driverairbag', $driverairbag, PDO::PARAM_STR);
+        $query->bindParam(':passengerairbag', $passengerairbag, PDO::PARAM_STR);
+        $query->bindParam(':powerwindow', $powerwindow, PDO::PARAM_STR);
+        $query->bindParam(':cdplayer', $cdplayer, PDO::PARAM_STR);
+        $query->bindParam(':centrallocking', $centrallocking, PDO::PARAM_STR);
+        $query->bindParam(':crashcensor', $crashcensor, PDO::PARAM_STR);
+        $query->bindParam(':leatherseats', $leatherseats, PDO::PARAM_STR);
+        $query->execute();
+        $lastInsertId = $dbh->lastInsertId();
+        if ($lastInsertId) {
+            $msg = "Vehicle posted successfully";
+        } else {
+            $error = "Something went wrong. Please try again";
+        }
+    }
     ?>
     <!DOCTYPE html>
     <html>
@@ -79,13 +145,40 @@ if (strlen($_SESSION['alogin']) == 0) {
                                                 <p class="text-primary m-0 font-weight-bold">Add a post</p>
                                             </div>
                                             <div class="card-body">
-                                                <form>
+                                                <form method="post" enctype="multipart/form-data">
                                                     <div class="form-row">
                                                         <div class="col">
                                                             <div class="form-group"><label
-                                                                        for="address"><strong>Title</strong></label><input
+                                                                        for="title"><strong>Title</strong></label><input
                                                                         class="form-control" type="text"
-                                                                        placeholder="Sunset Blvd, 38" name="address"></div>
+                                                                        placeholder="Enter Title" name="title">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-row">
+                                                        <div class="col">
+                                                            <div class="form-group">
+                                                                <label for="selectcat"><strong>Select Category</strong></label>
+                                                                <div class="">
+                                                                    <select class="form-control" name="selectcat" required>
+                                                                        <option value="">Select</option>
+                                                                        <?php $ret = "SELECT `id`,`catname` FROM `categories`";
+                                                                        $query = $dbh->prepare($ret);
+                                                                        //$query->bindParam(':id',$id, PDO::PARAM_STR);
+                                                                        $query->execute();
+                                                                        $results = $query->fetchAll(PDO::FETCH_OBJ);
+                                                                        if ($query->rowCount() > 0) {
+                                                                            foreach ($results as $result) {
+                                                                                ?>
+                                                                                <option value="<?php echo htmlentities($result->id); ?>">
+                                                                                    <?php echo htmlentities($result->catname); ?>
+                                                                                </option>
+                                                                            <?php }
+                                                                        } ?>
+
+                                                                    </select>
+                                                                </div>
+                                                            </div>
                                                         </div>
                                                     </div>
 
@@ -94,7 +187,8 @@ if (strlen($_SESSION['alogin']) == 0) {
                                                             <div class="form-group" style="width: 564px;"><label
                                                                         for="signature"><strong>Description</strong><br></label><textarea
                                                                         class="form-control form-control-lg" rows="4"
-                                                                        name="signature" style="height: 300px;"></textarea>
+                                                                        name="signature"
+                                                                        style="height: 300px;"></textarea>
                                                             </div>
                                                         </div>
                                                     </div>
@@ -102,7 +196,8 @@ if (strlen($_SESSION['alogin']) == 0) {
                                                     <div class="form-row">
                                                         <div class="col">
                                                             <div class="form-group">
-                                                                <button class="btn btn-primary btn-sm" type="submit">Post
+                                                                <button class="btn btn-primary btn-sm" type="submit" name="submit">
+                                                                    Post
                                                                 </button>
                                                             </div>
                                                         </div>
