5d51842914694f7d348673bb798a862c84101ec9
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-01 18:54:47 +0600

    Update add-post.php

--- admin/add-post.php
+++ admin/add-post.php
@@ -6,66 +6,19 @@ if (strlen($_SESSION['alogin']) == 0) {
 } else {
     if (isset($_POST['submit'])) {
         $title = $_POST['title'];
-        $brand = $_POST['brandname'];
-        $vehicleoverview = $_POST['vehicalorcview'];
-        $priceperday = $_POST['priceperday'];
-        $fueltype = $_POST['fueltype'];
-        $modelyear = $_POST['modelyear'];
-        $seatingcapacity = $_POST['seatingcapacity'];
-        $vimage1 = $_FILES["img1"]["name"];
-        $vimage2 = $_FILES["img2"]["name"];
-        $vimage3 = $_FILES["img3"]["name"];
-        $vimage4 = $_FILES["img4"]["name"];
-        $vimage5 = $_FILES["img5"]["name"];
-        $airconditioner = $_POST['airconditioner'];
-        $powerdoorlocks = $_POST['powerdoorlocks'];
-        $antilockbrakingsys = $_POST['antilockbrakingsys'];
-        $brakeassist = $_POST['brakeassist'];
-        $powersteering = $_POST['powersteering'];
-        $driverairbag = $_POST['driverairbag'];
-        $passengerairbag = $_POST['passengerairbag'];
-        $powerwindow = $_POST['powerwindow'];
-        $cdplayer = $_POST['cdplayer'];
-        $centrallocking = $_POST['centrallocking'];
-        $crashcensor = $_POST['crashcensor'];
-        $leatherseats = $_POST['leatherseats'];
+        $cat = $_POST['selectcat'];
+        $description = $_POST['description'];
 
-        move_uploaded_file($_FILES["img1"]["tmp_name"], "img/vehicleimages/" . $_FILES["img1"]["name"]);
-        move_uploaded_file($_FILES["img2"]["tmp_name"], "img/vehicleimages/" . $_FILES["img2"]["name"]);
-        move_uploaded_file($_FILES["img3"]["tmp_name"], "img/vehicleimages/" . $_FILES["img3"]["name"]);
-        move_uploaded_file($_FILES["img4"]["tmp_name"], "img/vehicleimages/" . $_FILES["img4"]["name"]);
-        move_uploaded_file($_FILES["img5"]["tmp_name"], "img/vehicleimages/" . $_FILES["img5"]["name"]);
-
-        $sql = "INSERT INTO tblvehicles(VehiclesTitle,VehiclesBrand,VehiclesOverview,PricePerDay,FuelType,ModelYear,SeatingCapacity,Vimage1,Vimage2,Vimage3,Vimage4,Vimage5,AirConditioner,PowerDoorLocks,AntiLockBrakingSystem,BrakeAssist,PowerSteering,DriverAirbag,PassengerAirbag,PowerWindows,CDPlayer,CentralLocking,CrashSensor,LeatherSeats) VALUES(:vehicletitle,:brand,:vehicleoverview,:priceperday,:fueltype,:modelyear,:seatingcapacity,:vimage1,:vimage2,:vimage3,:vimage4,:vimage5,:airconditioner,:powerdoorlocks,:antilockbrakingsys,:brakeassist,:powersteering,:driverairbag,:passengerairbag,:powerwindow,:cdplayer,:centrallocking,:crashcensor,:leatherseats)";
+        $sql = "INSERT INTO posts(title,category,description) VALUES(:title,:cat,:description)";
         $query = $dbh->prepare($sql);
-        $query->bindParam(':vehicletitle', $vehicletitle, PDO::PARAM_STR);
-        $query->bindParam(':brand', $brand, PDO::PARAM_STR);
-        $query->bindParam(':vehicleoverview', $vehicleoverview, PDO::PARAM_STR);
-        $query->bindParam(':priceperday', $priceperday, PDO::PARAM_STR);
-        $query->bindParam(':fueltype', $fueltype, PDO::PARAM_STR);
-        $query->bindParam(':modelyear', $modelyear, PDO::PARAM_STR);
-        $query->bindParam(':seatingcapacity', $seatingcapacity, PDO::PARAM_STR);
-        $query->bindParam(':vimage1', $vimage1, PDO::PARAM_STR);
-        $query->bindParam(':vimage2', $vimage2, PDO::PARAM_STR);
-        $query->bindParam(':vimage3', $vimage3, PDO::PARAM_STR);
-        $query->bindParam(':vimage4', $vimage4, PDO::PARAM_STR);
-        $query->bindParam(':vimage5', $vimage5, PDO::PARAM_STR);
-        $query->bindParam(':airconditioner', $airconditioner, PDO::PARAM_STR);
-        $query->bindParam(':powerdoorlocks', $powerdoorlocks, PDO::PARAM_STR);
-        $query->bindParam(':antilockbrakingsys', $antilockbrakingsys, PDO::PARAM_STR);
-        $query->bindParam(':brakeassist', $brakeassist, PDO::PARAM_STR);
-        $query->bindParam(':powersteering', $powersteering, PDO::PARAM_STR);
-        $query->bindParam(':driverairbag', $driverairbag, PDO::PARAM_STR);
-        $query->bindParam(':passengerairbag', $passengerairbag, PDO::PARAM_STR);
-        $query->bindParam(':powerwindow', $powerwindow, PDO::PARAM_STR);
-        $query->bindParam(':cdplayer', $cdplayer, PDO::PARAM_STR);
-        $query->bindParam(':centrallocking', $centrallocking, PDO::PARAM_STR);
-        $query->bindParam(':crashcensor', $crashcensor, PDO::PARAM_STR);
-        $query->bindParam(':leatherseats', $leatherseats, PDO::PARAM_STR);
+        $query->bindParam(':title', $title, PDO::PARAM_STR);
+        $query->bindParam(':cat', $cat, PDO::PARAM_STR);
+        $query->bindParam(':description', $description, PDO::PARAM_STR);
+
         $query->execute();
         $lastInsertId = $dbh->lastInsertId();
         if ($lastInsertId) {
-            $msg = "Vehicle posted successfully";
+            $msg = "Blog Posted successfully";
         } else {
             $error = "Something went wrong. Please try again";
         }
@@ -175,7 +128,6 @@ if (strlen($_SESSION['alogin']) == 0) {
                                                                                 </option>
                                                                             <?php }
                                                                         } ?>
-
                                                                     </select>
                                                                 </div>
                                                             </div>
