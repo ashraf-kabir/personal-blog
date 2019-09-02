3a76f92bd6ad659d42e9dbe877677670a08b9a3f
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-02 23:21:06 +0600

    repeated codes are removed

--- admin/add-post.php
+++ admin/add-post.php
@@ -53,44 +53,6 @@ if (strlen($_SESSION['alogin']) == 0) {
                         <h3 class="text-dark mb-4">Add a Post</h3>
                         <div class="row mb-3">
                             <div class="col-lg-8">
-                                <div class="row mb-3 d-none">
-                                    <div class="col">
-                                        <div class="card text-white bg-primary shadow">
-                                            <div class="card-body">
-                                                <div class="row mb-2">
-                                                    <div class="col">
-                                                        <p class="m-0">Performance</p>
-                                                        <p class="m-0"><strong>65.2%</strong></p>
-                                                    </div>
-                                                    <div class="col-auto"><i class="fas fa-rocket fa-2x"></i></div>
-                                                </div>
-                                                <p class="text-white-50 small m-0"><i class="fas fa-arrow-up"></i>&nbsp;5%
-                                                                                                                  since
-                                                                                                                  last
-                                                                                                                  month
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col">
-                                        <div class="card text-white bg-success shadow">
-                                            <div class="card-body">
-                                                <div class="row mb-2">
-                                                    <div class="col">
-                                                        <p class="m-0">Performance</p>
-                                                        <p class="m-0"><strong>65.2%</strong></p>
-                                                    </div>
-                                                    <div class="col-auto"><i class="fas fa-rocket fa-2x"></i></div>
-                                                </div>
-                                                <p class="text-white-50 small m-0"><i class="fas fa-arrow-up"></i>&nbsp;5%
-                                                                                                                  since
-                                                                                                                  last
-                                                                                                                  month
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
 
                                 <div class="row">
                                     <div class="col">
