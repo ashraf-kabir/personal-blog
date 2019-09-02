903c8bf65516b56a33399da763f205213d528804
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-03 01:36:44 +0600

    manage-pages.php added

--- about.php
+++ about.php
@@ -19,15 +19,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-lg-8 mx-auto">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe nostrum ullam eveniet pariatur
-                   voluptates odit, fuga atque ea nobis sit soluta odio, adipisci quas excepturi maxime quae totam
-                   ducimus consectetur?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius praesentium recusandae illo eaque
-                   architecto error, repellendus iusto reprehenderit, doloribus, minus sunt. Numquam at quae voluptatum
-                   in officia voluptas voluptatibus, minus!</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequuntur magnam, excepturi aliquid
-                   ex itaque esse est vero natus quae optio aperiam soluta voluptatibus corporis atque iste neque sit
-                   tempora!</p>
+                <p></p>
             </div>
         </div>
     </div>
