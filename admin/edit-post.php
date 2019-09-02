28947d57ed433cf066e730b08131670493803e8c
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-02 23:16:05 +0600

    Update edit-post.php

--- admin/edit-post.php
+++ admin/edit-post.php
@@ -51,6 +51,7 @@ if (strlen($_SESSION['alogin']) == 0) {
                         <div class="row mb-3">
                             <div class="col-lg-8">
                                 <div class="row mb-3 d-none">
+
                                     <div class="col">
                                         <div class="card text-white bg-primary shadow">
                                             <div class="card-body">
@@ -69,24 +70,7 @@ if (strlen($_SESSION['alogin']) == 0) {
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col">
-                                        <div class="card text-white bg-success shadow">
-                                            <div class="card-body">
-                                                <div class="row mb-2">
-                                                    <div class="col">
-                                                        <p class="m-0">Performance</p>
-                                                        <p class="m-0"><strong>65.2%</strong></p>
-                                                    </div>
-                                                    <div class="col-auto"><i class="fas fa-rocket fa-2x"></i></div>
-                                                </div>
-                                                <p class="text-white-50 small m-0"><i class="fas fa-arrow-up"></i>&nbsp;5%
-                                                                                                                  since
-                                                                                                                  last
-                                                                                                                  month
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
+
                                 </div>
 
                                 <div class="row">
