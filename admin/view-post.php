d3630dc5757a2245a87da7d46704e09a846e97fd
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-02 11:23:00 +0600

    Update view-post.php

--- admin/view-post.php
+++ admin/view-post.php
@@ -4,6 +4,14 @@ include('includes/config.php');
 if (strlen($_SESSION['alogin']) == 0) {
     header('location: login.php');
 } else {
+    if (isset($_REQUEST['del'])) {
+        $delid = intval($_GET['del']);
+        $sql = "DELETE FROM posts WHERE id=:delid";
+        $query = $dbh->prepare($sql);
+        $query->bindParam(':delid', $delid, PDO::PARAM_STR);
+        $query->execute();
+        $msg = "Post deleted successfully";
+    }
     ?>
     <!DOCTYPE html>
     <html>
@@ -31,7 +39,7 @@ if (strlen($_SESSION['alogin']) == 0) {
                     <?php include 'includes/header.php'; ?>
 
                     <div class="container-fluid">
-                        <h3 class="text-dark mb-4">Team</h3>
+                        <h3 class="text-dark mb-4">Posts</h3>
                         <div class="card shadow">
                             <div class="card-header py-3">
                                 <p class="text-primary m-0 font-weight-bold">Employee Info</p>
@@ -42,10 +50,8 @@ if (strlen($_SESSION['alogin']) == 0) {
                                         <div id="dataTable_length" class="dataTables_length" aria-controls="dataTable">
                                             <label>Show&nbsp;<select
                                                         class="form-control form-control-sm custom-select custom-select-sm">
-                                                    <option value="10" selected="">10</option>
-                                                    <option value="25">25</option>
-                                                    <option value="50">50</option>
-                                                    <option value="100">100</option>
+                                                    <option value="5" selected="">5</option>
+                                                    <option value="10">10</option>
                                                 </select>&nbsp;</label></div>
                                     </div>
                                     <div class="col-md-6">
@@ -59,123 +65,43 @@ if (strlen($_SESSION['alogin']) == 0) {
                                     <table class="table dataTable my-0" id="dataTable">
                                         <thead>
                                         <tr>
-                                            <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>
-                                            <th>Start date</th>
-                                            <th>Salary</th>
+                                            <th>ID</th>
+                                            <th>Title</th>
+                                            <th>Category</th>
+                                            <th>Edit</th>
+                                            <th>Delete</th>
                                         </tr>
                                         </thead>
                                         <tbody>
-                                        <tr>
-                                            <td><img class="rounded-circle mr-2" width="30" height="30"
-                                                     src="assets/img/avatars/avatar1.jpeg">Airi Satou
-                                            </td>
-                                            <td>Accountant</td>
-                                            <td>Tokyo</td>
-                                            <td>33</td>
-                                            <td>2008/11/28</td>
-                                            <td>$162,700</td>
-                                        </tr>
-                                        <tr>
-                                            <td><img class="rounded-circle mr-2" width="30" height="30"
-                                                     src="assets/img/avatars/avatar2.jpeg">Angelica Ramos
-                                            </td>
-                                            <td>Chief Executive Officer(CEO)</td>
-                                            <td>London</td>
-                                            <td>47</td>
-                                            <td>2009/10/09<br></td>
-                                            <td>$1,200,000</td>
-                                        </tr>
-                                        <tr>
-                                            <td><img class="rounded-circle mr-2" width="30" height="30"
-                                                     src="assets/img/avatars/avatar3.jpeg">Ashton Cox
-                                            </td>
-                                            <td>Junior Technical Author</td>
-                                            <td>San Francisco</td>
-                                            <td>66</td>
-                                            <td>2009/01/12<br></td>
-                                            <td>$86,000</td>
-                                        </tr>
-                                        <tr>
-                                            <td><img class="rounded-circle mr-2" width="30" height="30"
-                                                     src="assets/img/avatars/avatar4.jpeg">Bradley Greer
-                                            </td>
-                                            <td>Software Engineer</td>
-                                            <td>London</td>
-                                            <td>41</td>
-                                            <td>2012/10/13<br></td>
-                                            <td>$132,000</td>
-                                        </tr>
-                                        <tr>
-                                            <td><img class="rounded-circle mr-2" width="30" height="30"
-                                                     src="assets/img/avatars/avatar5.jpeg">Brenden Wagner
-                                            </td>
-                                            <td>Software Engineer</td>
-                                            <td>San Francisco</td>
-                                            <td>28</td>
-                                            <td>2011/06/07<br></td>
-                                            <td>$206,850</td>
-                                        </tr>
-                                        <tr>
-                                            <td><img class="rounded-circle mr-2" width="30" height="30"
-                                                     src="assets/img/avatars/avatar1.jpeg">Brielle Williamson
-                                            </td>
-                                            <td>Integration Specialist</td>
-                                            <td>New York</td>
-                                            <td>61</td>
-                                            <td>2012/12/02<br></td>
-                                            <td>$372,000</td>
-                                        </tr>
-                                        <tr>
-                                            <td><img class="rounded-circle mr-2" width="30" height="30"
-                                                     src="assets/img/avatars/avatar2.jpeg">Bruno Nash<br></td>
-                                            <td>Software Engineer</td>
-                                            <td>London</td>
-                                            <td>38</td>
-                                            <td>2011/05/03<br></td>
-                                            <td>$163,500</td>
-                                        </tr>
-                                        <tr>
-                                            <td><img class="rounded-circle mr-2" width="30" height="30"
-                                                     src="assets/img/avatars/avatar3.jpeg">Caesar Vance
-                                            </td>
-                                            <td>Pre-Sales Support</td>
-                                            <td>New York</td>
-                                            <td>21</td>
-                                            <td>2011/12/12<br></td>
-                                            <td>$106,450</td>
-                                        </tr>
-                                        <tr>
-                                            <td><img class="rounded-circle mr-2" width="30" height="30"
-                                                     src="assets/img/avatars/avatar4.jpeg">Cara Stevens
-                                            </td>
-                                            <td>Sales Assistant</td>
-                                            <td>New York</td>
-                                            <td>46</td>
-                                            <td>2011/12/06<br></td>
-                                            <td>$145,600</td>
-                                        </tr>
-                                        <tr>
-                                            <td><img class="rounded-circle mr-2" width="30" height="30"
-                                                     src="assets/img/avatars/avatar5.jpeg">Cedric Kelly
-                                            </td>
-                                            <td>Senior JavaScript Developer</td>
-                                            <td>Edinburgh</td>
-                                            <td>22</td>
-                                            <td>2012/03/29<br></td>
-                                            <td>$433,060</td>
-                                        </tr>
+
+                                        <?php $sql = "SELECT posts.title,categories.catname,posts.id from posts join categories on categories.id=posts.category";
+                                        $query = $dbh->prepare($sql);
+                                        $query->execute();
+                                        $results = $query->fetchAll(PDO::FETCH_OBJ);
+                                        $cnt = 1;
+                                        if ($query->rowCount() > 0) {
+                                            foreach ($results as $result) { ?>
+                                                <tr>
+                                                    <td><?php echo htmlentities($cnt); ?></td>
+                                                    <td><?php echo htmlentities($result->title); ?></td>
+                                                    <td><?php echo htmlentities($result->catname); ?></td>
+                                                    <td><a href="edit-post.php?id=<?php echo $result->id; ?>">edit</a>
+                                                    <td><a href="view-post.php?del=<?php echo $result->id; ?>"
+                                                           onclick="return confirm('Do you want to delete?');">delete</a>
+                                                    </td>
+                                                </tr>
+                                                <?php $cnt = $cnt + 1;
+                                            }
+                                        } ?>
+
                                         </tbody>
                                         <tfoot>
                                         <tr>
-                                            <td><strong>Name</strong></td>
-                                            <td><strong>Position</strong></td>
-                                            <td><strong>Office</strong></td>
-                                            <td><strong>Age</strong></td>
-                                            <td><strong>Start date</strong></td>
-                                            <td><strong>Salary</strong></td>
+                                            <td><strong>ID</strong></td>
+                                            <td><strong>Title</strong></td>
+                                            <td><strong>Category</strong></td>
+                                            <td><strong>Edit</strong></td>
+                                            <td><strong>Delete</strong></td>
                                         </tr>
                                         </tfoot>
                                     </table>
@@ -183,7 +109,7 @@ if (strlen($_SESSION['alogin']) == 0) {
                                 <div class="row">
                                     <div class="col-md-6 align-self-center">
                                         <p id="dataTable_info" class="dataTables_info" role="status" aria-live="polite">
-                                            Showing 1 to 10 of 27</p>
+                                            Showing 1 to 5 of 100</p>
                                     </div>
                                     <div class="col-md-6">
                                         <nav class="d-lg-flex justify-content-lg-end dataTables_paginate paging_simple_numbers">
