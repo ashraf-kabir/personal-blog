294c9de05ef847c2597d352217a0774e0ad5403b
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-03 11:38:29 +0600

    Update manage-pages.php

--- admin/manage-pages.php
+++ admin/manage-pages.php
@@ -12,7 +12,7 @@ if (strlen($_SESSION['alogin']) == 0) {
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-        <title>Posts - Manage Posts</title>
+        <title>Pages - Manage Pages</title>
         <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet"
               href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
@@ -32,27 +32,12 @@ if (strlen($_SESSION['alogin']) == 0) {
                     <?php include 'includes/header.php'; ?>
 
                     <div class="container-fluid">
-                        <h3 class="text-dark mb-4">Posts</h3>
+                        <h3 class="text-dark mb-4">Pages</h3>
                         <div class="card shadow">
                             <div class="card-header py-3">
-                                <p class="text-primary m-0 font-weight-bold">Posts</p>
+                                <p class="text-primary m-0 font-weight-bold">Pages</p>
                             </div>
                             <div class="card-body">
-                                <div class="row">
-                                    <div class="col-md-6 text-nowrap">
-                                        <div id="dataTable_length" class="dataTables_length" aria-controls="dataTable">
-                                            <label>Show&nbsp;<select
-                                                        class="form-control form-control-sm custom-select custom-select-sm">
-                                                    <option value="5" selected="">5</option>
-                                                    <option value="10">10</option>
-                                                </select>&nbsp;</label></div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="text-md-right dataTables_filter" id="dataTable_filter"><label><input
-                                                        type="search" class="form-control form-control-sm"
-                                                        aria-controls="dataTable" placeholder="Search"></label></div>
-                                    </div>
-                                </div>
                                 <div class="table-responsive table mt-2" id="dataTable" role="grid"
                                      aria-describedby="dataTable_info">
                                     <table class="table dataTable my-0" id="dataTable">
@@ -93,27 +78,6 @@ if (strlen($_SESSION['alogin']) == 0) {
                                         </tr>
                                         </tfoot>
                                     </table>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6 align-self-center">
-                                        <p id="dataTable_info" class="dataTables_info" role="status" aria-live="polite">
-                                            Showing 1 to 5 of 100</p>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <nav class="d-lg-flex justify-content-lg-end dataTables_paginate paging_simple_numbers">
-                                            <ul class="pagination">
-                                                <li class="page-item disabled"><a class="page-link" href="#"
-                                                                                  aria-label="Previous"><span
-                                                                aria-hidden="true">«</span></a></li>
-                                                <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                                                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                                <li class="page-item"><a class="page-link" href="#"
-                                                                         aria-label="Next"><span
-                                                                aria-hidden="true">»</span></a></li>
-                                            </ul>
-                                        </nav>
-                                    </div>
                                 </div>
                             </div>
                         </div>
