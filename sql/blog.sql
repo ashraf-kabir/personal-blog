a4a0d358059c8370772a7893f6a85db2c920ff89
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-06 19:50:39 +0600

    Update blog.sql

--- sql/blog.sql
+++ sql/blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 01, 2019 at 03:42 PM
+-- Generation Time: Sep 06, 2019 at 03:48 PM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.3.7
 
@@ -66,15 +66,70 @@ INSERT INTO `categories` (`id`, `catname`, `creationdate`, `updationdate`) VALUE
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(100) NOT NULL,
+  `postid` int(100) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `comment` mediumtext NOT NULL,
+  `postingdate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `postid`, `name`, `email`, `comment`, `postingdate`, `status`) VALUES
+(1, 1, 'Ashraf Kabir', 'ashrafkabir95@gmail.com', 'Test', '2019-09-06 11:10:28', 1),
+(2, 2, 'Ashraf Kabir', 'ashrafkabir95@gmail.com', 'something', '2019-09-06 11:16:52', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contactusquery`
+--
+
+CREATE TABLE `contactusquery` (
+  `id` int(11) NOT NULL,
+  `uname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `contact` varchar(255) NOT NULL,
+  `message` longtext NOT NULL,
+  `postingdate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `contactusquery`
+--
+
+INSERT INTO `contactusquery` (`id`, `uname`, `email`, `contact`, `message`, `postingdate`, `status`) VALUES
+(1, 'Ashraf Kabir', 'ashrafkabir95@gmail.com', '01751336666', 'Something important', '2019-09-03 12:12:19', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pages`
 --
 
 CREATE TABLE `pages` (
   `id` int(11) NOT NULL,
   `pagename` varchar(30) NOT NULL,
-  `type` varchar(10) NOT NULL,
+  `pagetype` varchar(10) NOT NULL,
   `description` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pages`
+--
+
+INSERT INTO `pages` (`id`, `pagename`, `pagetype`, `description`) VALUES
+(1, 'About Us', 'aboutus', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\",'),
+(2, 'Contact Us', 'contactus', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.');
 
 -- --------------------------------------------------------
 
@@ -85,6 +140,7 @@ CREATE TABLE `pages` (
 CREATE TABLE `posts` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
+  `grabber` varchar(255) NOT NULL,
   `category` varchar(255) NOT NULL,
   `description` longtext NOT NULL,
   `creationdate` timestamp NOT NULL DEFAULT current_timestamp(),
@@ -95,9 +151,10 @@ CREATE TABLE `posts` (
 -- Dumping data for table `posts`
 --
 
-INSERT INTO `posts` (`id`, `title`, `category`, `description`, `creationdate`, `updationdate`) VALUES
-(1, 'Test001', '1', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2019-09-01 13:01:13', NULL),
-(2, 'Test002', '2', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2019-09-01 13:03:05', NULL);
+INSERT INTO `posts` (`id`, `title`, `grabber`, `category`, `description`, `creationdate`, `updationdate`) VALUES
+(1, 'Test001', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable.', '1', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2019-09-01 13:01:13', '2019-09-03 18:43:33'),
+(2, 'Test002', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.', '2', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,', '2019-09-01 13:03:05', '2019-09-03 18:44:02'),
+(5, 'Test003', '', '1', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy.', '2019-09-06 13:27:45', NULL);
 
 -- --------------------------------------------------------
 
@@ -107,10 +164,19 @@ INSERT INTO `posts` (`id`, `title`, `category`, `description`, `creationdate`, `
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `username` varchar(30) NOT NULL,
+  `fname` varchar(15) NOT NULL,
+  `lname` varchar(15) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `fname`, `lname`, `email`, `password`) VALUES
+(1, 'user', '123', 'user@gmail.com', 'ee11cbb19052e40b07aac0ca060c23ee'),
+(2, 'Ashraf', 'Kabir', 'ashrafkabir95@gmail.com', 'ee11cbb19052e40b07aac0ca060c23ee');
 
 --
 -- Indexes for dumped tables
@@ -126,6 +192,18 @@ ALTER TABLE `admin`
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contactusquery`
+--
+ALTER TABLE `contactusquery`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -163,22 +241,34 @@ ALTER TABLE `categories`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `contactusquery`
+--
+ALTER TABLE `contactusquery`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `pages`
 --
 ALTER TABLE `pages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
