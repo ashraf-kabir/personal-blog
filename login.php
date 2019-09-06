76bc0491a3ed76b36a9d4585dd6bd0909ebf2da2
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-06 11:23:25 +0600

    Update login.php

--- login.php
+++ login.php
@@ -29,7 +29,7 @@ if (isset($_POST['login'])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Login - Admin</title>
+    <title>Login - User</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
@@ -48,7 +48,7 @@ if (isset($_POST['login'])) {
                         <div class="row">
                             <div class="col-lg-6 d-none d-lg-flex">
                                 <div class="flex-grow-1 bg-login-image"
-                                     style="background-image: url(&quot;assets/img/sample/Spongebob-Squarepants.jpg&quot;);"></div>
+                                     style="background-image: url(&quot;admin/assets/img/sample/Spongebob-Squarepants.jpg&quot;);"></div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="p-5">
