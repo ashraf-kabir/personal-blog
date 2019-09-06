9a44ff1a9c9e758726068c8e021c16b96d558310
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-06 13:55:57 +0600

    Update login.php

--- login.php
+++ login.php
@@ -30,12 +30,12 @@ if (isset($_POST['login'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Login - User</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="admin/assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
     <link rel="stylesheet" href="/admin/assets/fonts/fontawesome-all.min.css">
 
-    <script src="assets/js/jquery.min.js"></script>
+    <script src="admin/assets/js/jquery.min.js"></script>
 
 </head>
 
@@ -89,8 +89,8 @@ if (isset($_POST['login'])) {
             </div>
         </div>
     </div>
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="admin/assets/js/jquery.min.js"></script>
+    <script src="admin/assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="admin/assets/js/chart.min.js"></script>
     <script src="admin/assets/js/bs-charts.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
