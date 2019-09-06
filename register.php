c19aa02a1c2bbb38d01baa73169efc33a11b0a46
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-06 14:03:36 +0600

    Update register.php

--- register.php
+++ register.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Register - Brand</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="admin/assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
     <link rel="stylesheet" href="admin/assets/fonts/fontawesome-all.min.css">
@@ -51,11 +51,6 @@
                                 <button class="btn btn-primary btn-block text-white btn-user" type="submit">Register
                                                                                                             Account
                                 </button>
-                                <hr>
-                                <a class="btn btn-primary btn-block text-white btn-google btn-user" role="button"><i
-                                        class="fab fa-google"></i>&nbsp; Register with Google</a><a
-                                    class="btn btn-primary btn-block text-white btn-facebook btn-user"
-                                    role="button"><i class="fab fa-facebook-f"></i>&nbsp; Register with Facebook</a>
                                 <hr>
                             </form>
                             <div class="text-center"><a class="small" href="forgot-password.php">Forgot Password?</a>
