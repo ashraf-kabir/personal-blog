62e29c3eea8cb753be11b2374cb440a3110b0c12
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-07 15:29:45 +0600

    Update profile.php

--- profile.php
+++ profile.php
@@ -4,6 +4,24 @@ include('includes/config.php');
 if (strlen($_SESSION['login']) == 0) {
     header('location: login.php');
 } else {
+    if (isset($_POST['submit'])) {
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $id = intval($_GET['id']);
+
+        $sql1 = "UPDATE `users` SET fname=:fname,lname=:lname,email=:email,phone=:phone WHERE id=:id ";
+        $query = $dbh->prepare($sql1);
+        $query->bindParam(':fname', $fname, PDO::PARAM_STR);
+        $query->bindParam(':lname', $lname, PDO::PARAM_STR);
+        $query->bindParam(':email', $email, PDO::PARAM_STR);
+        $query->bindParam(':phone', $phone, PDO::PARAM_STR);
+        $query->bindParam(':id', $id, PDO::PARAM_STR);
+        $query->execute();
+
+        echo "<script>alert('User updated');</script>";
+    }
     ?>
     <!DOCTYPE html>
     <html>
@@ -25,33 +43,49 @@ if (strlen($_SESSION['login']) == 0) {
 
         <div class="container">
             <div class="row">
-                <form>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                    </div>
-                    <div class="form-group form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
-            </div>
-
-            <div class="row">
                 <div class="col-md-10 col-lg-8">
                     <div class="clearfix">
-                        <a href="post.php">
-                            <button class="btn btn-primary float-right" type="button">Older Posts&nbsp;⇒</button>
-                        </a>
+                        <h4>Update Profile</h4>
+                        <br>
+                        <?php
+                        $email = $_SESSION['login'];
+                        $sql2 = "SELECT * FROM users WHERE email=:email";
+                        $query = $dbh->prepare($sql2);
+                        $query->bindParam(':email', $email, PDO::PARAM_STR);
+                        $query->execute();
+                        $results = $query->fetchAll(PDO::FETCH_OBJ);
+                        $cnt = 1;
+                        if ($query->rowCount() > 0) {
+                        foreach ($results
+
+                        as $result) { ?>
+                        <form method="post">
+                            <div class="form-group">
+                                <label for="fname">First Name</label>
+                                <input type="text" class="form-control" id="fname" name="fname" value="<?php echo htmlentities($result->fname); ?>">
+                            </div>
+                            <div class="form-group">
+                                <label for="lname">Last Name</label>
+                                <input type="text" class="form-control" id="lname" name="lname" value="<?php echo htmlentities($result->lname); ?>">
+                            </div>
+                            <div class="form-group">
+                                <label for="email1">Email address</label>
+                                <input type="email" class="form-control" id="email1" name="email" value="<?php echo htmlentities($result->email); ?>">
+                            </div>
+                            <div class="form-group">
+                                <label for="phone">Phone</label>
+                                <input type="tel" class="form-control" id="phone" name="phone" value="<?php echo htmlentities($result->phone); ?>">
+                            </div>
+                            <?php }
+                            } ?>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary" name="submit">Update</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
+
         </div>
 
         <!-- Footer -->
