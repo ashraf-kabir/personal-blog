6c51097794e3cc9414cf2ba57cb33522b7401f9a
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-07 15:08:41 +0600

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,51 +1,66 @@
 <?php
 session_start();
 include('includes/config.php');
-error_reporting(0);
-$_SESSION['redirectURL'] = $_SERVER['REQUEST_URI'];
-?>
-<!DOCTYPE html>
-<html>
+if (strlen($_SESSION['login']) == 0) {
+    header('location: login.php');
+} else {
+    ?>
+    <!DOCTYPE html>
+    <html>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Profile</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic">
-    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <title>Profile</title>
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet"
+              href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic">
+        <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    </head>
 
-<body>
-    <!-- Header -->
-    <?php include 'includes/header.php'; ?>
+    <body>
+        <!-- Header -->
+        <?php include 'includes/header.php'; ?>
 
-    <div class="container">
-        <div class="row">
+        <div class="container">
+            <div class="row">
+                <form>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
 
-
-
-        </div>
-
-        <div class="row">
-            <div class="col-md-10 col-lg-8">
-                <div class="clearfix">
-                    <a href="post.php">
-                        <button class="btn btn-primary float-right" type="button">Older Posts&nbsp;⇒</button>
-                    </a>
+            <div class="row">
+                <div class="col-md-10 col-lg-8">
+                    <div class="clearfix">
+                        <a href="post.php">
+                            <button class="btn btn-primary float-right" type="button">Older Posts&nbsp;⇒</button>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <!-- Footer -->
-    <?php include 'includes/footer.php'; ?>
+        <!-- Footer -->
+        <?php include 'includes/footer.php'; ?>
 
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="assets/js/clean-blog.js"></script>
-</body>
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="assets/js/clean-blog.js"></script>
+    </body>
 
-</html>
+    </html>
+<?php } ?>
