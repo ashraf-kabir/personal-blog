b192532d32b2630b94ea4d8a8be55df50dec9e2a
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-07 16:06:17 +0600

    Update update-password.php

--- update-password.php
+++ update-password.php
@@ -101,11 +101,7 @@ if (isset($_POST['submit'])) {
                                     <label class="col-md-4 control-label">&nbsp;</label>
                                     <div class="col-md-4">
 
-                                        <button type="submit"
-                                                class="btn btn-primary"
-                                                name="submit">
-                                            Submit
-                                        </button>
+                                        <button type="submit" class="btn btn-danger" name="submit">Submit</button>
                                     </div>
                                 </div>
 
