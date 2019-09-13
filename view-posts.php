c917effd29bc3bacd8a3d9d662a48957284bbd77
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-13 15:09:01 +0600

    Update view-posts.php

--- view-posts.php
+++ view-posts.php
@@ -38,8 +38,11 @@ $_SESSION['redirectURL'] = $_SERVER['REQUEST_URI'];
     <article>
         <div class="container">
             <div class="row">
-                <?php $sql = "SELECT posts.*,categories.catname FROM posts JOIN categories ON categories.id=posts.category ORDER BY posts.id DESC";
+                <?php
+                $s = 1;
+                $sql = "SELECT posts.*,categories.catname FROM posts JOIN categories ON categories.id=posts.category WHERE posts.status=:s ORDER BY posts.id DESC";
                 $query = $dbh->prepare($sql);
+                $query->bindParam(':s', $s, PDO::PARAM_STR);
                 $query->execute();
                 $results = $query->fetchAll(PDO::FETCH_OBJ);
                 $cnt = 1;
