1631830701d1ee2d895e48c33fd4ecdfb0d1a8ec
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-09-13 14:00:41 +0600

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -3,12 +3,15 @@
         <div class="row">
             <div class="col-md-10 col-lg-8 mx-auto">
                 <ul class="list-inline text-center">
-                    <li class="list-inline-item"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i
-                                    class="fa fa-twitter fa-stack-1x fa-inverse"></i></span></li>
-                    <li class="list-inline-item"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i
-                                    class="fa fa-facebook fa-stack-1x fa-inverse"></i></span></li>
-                    <li class="list-inline-item"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i
-                                    class="fa fa-github fa-stack-1x fa-inverse"></i></span></li>
+                    <li class="list-inline-item"><a href="https://twitter.com/ashraf1Q95" target="_blank"><span
+                                    class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i
+                                        class="fa fa-twitter fa-stack-1x fa-inverse"></i></span></a></li>
+                    <li class="list-inline-item"><a href="https://www.facebook.com/sonnet404" target="_blank"><span
+                                    class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i
+                                        class="fa fa-facebook fa-stack-1x fa-inverse"></i></span></a></li>
+                    <li class="list-inline-item"><a href="https://github.com/ashraf-kabir" target="_blank"><span
+                                    class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i
+                                        class="fa fa-github fa-stack-1x fa-inverse"></i></span></a></li>
                 </ul>
                 <p class="text-muted copyright">Copyright&nbsp;©&nbsp;Ashraf Kabir 2019</p>
             </div>
