f7e4c2980f2d00212c87199781f7f4ea1e86d717
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-11-14 21:31:09 +0600

    Update manage-users.php

--- admin/manage-users.php
+++ admin/manage-users.php
@@ -10,8 +10,19 @@ if (strlen($_SESSION['alogin']) == 0) {
         $query = $dbh->prepare($sql);
         $query->bindParam(':delid', $delid, PDO::PARAM_STR);
         $query->execute();
-        echo "<script>alert('User deleted')</script>";
+        echo "<script>alert('User deleted');</script>";
     }
+
+    if (isset($_REQUEST['uaid'])) {
+        $uaid = intval($_GET['uaid']);
+        $sql = "UPDATE `users` SET `status`=1 WHERE id=:uaid";
+        $query = $dbh->prepare($sql);
+        $query->bindParam(':uaid', $uaid, PDO::PARAM_STR);
+        $query->execute();
+        echo "<script>alert('User APPROVED successfully');</script>";
+    }
+
+
     ?>
     <!DOCTYPE html>
     <html>
@@ -42,22 +53,22 @@ if (strlen($_SESSION['alogin']) == 0) {
                         <h3 class="text-dark mb-4">Users</h3>
                         <div class="card shadow">
                             <div class="card-header py-3">
-                                <p class="text-primary m-0 font-weight-bold">Users</p>
+                                <p class="text-primary m-0 font-weight-bold">Active Users</p>
                             </div>
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col-md-6 text-nowrap">
                                         <div id="dataTable_length" class="dataTables_length" aria-controls="dataTable">
                                             <label>Show&nbsp;<select
-                                                    class="form-control form-control-sm custom-select custom-select-sm">
+                                                        class="form-control form-control-sm custom-select custom-select-sm">
                                                     <option value="5" selected="">5</option>
                                                     <option value="10">10</option>
                                                 </select>&nbsp;</label></div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="text-md-right dataTables_filter" id="dataTable_filter"><label><input
-                                                    type="search" class="form-control form-control-sm"
-                                                    aria-controls="dataTable" placeholder="Search"></label></div>
+                                                        type="search" class="form-control form-control-sm"
+                                                        aria-controls="dataTable" placeholder="Search"></label></div>
                                     </div>
                                 </div>
                                 <div class="table-responsive table mt-2" id="dataTable" role="grid"
@@ -73,14 +84,14 @@ if (strlen($_SESSION['alogin']) == 0) {
                                         </thead>
                                         <tbody>
 
-                                        <?php $sql = "SELECT users.* FROM users";
+                                        <?php $sql = "SELECT users.* FROM users WHERE `status`=1";
                                         $query = $dbh->prepare($sql);
                                         $query->execute();
                                         $results = $query->fetchAll(PDO::FETCH_OBJ);
                                         $cnt = 1;
                                         if ($query->rowCount() > 0) {
                                             foreach ($results as $result) {
-                                                $name = htmlentities($result->fname." ".$result->lname);?>
+                                                $name = htmlentities($result->fname . " " . $result->lname); ?>
                                                 <tr>
                                                     <td><?php echo htmlentities($cnt); ?></td>
                                                     <td><?php echo htmlentities($name); ?></td>
@@ -106,19 +117,106 @@ if (strlen($_SESSION['alogin']) == 0) {
                                             <ul class="pagination">
                                                 <li class="page-item disabled"><a class="page-link" href="#"
                                                                                   aria-label="Previous"><span
-                                                            aria-hidden="true">«</span></a></li>
+                                                                aria-hidden="true">«</span></a></li>
                                                 <li class="page-item active"><a class="page-link" href="#">1</a></li>
                                                 <li class="page-item"><a class="page-link" href="#">2</a></li>
                                                 <li class="page-item"><a class="page-link" href="#">3</a></li>
                                                 <li class="page-item"><a class="page-link" href="#"
                                                                          aria-label="Next"><span
-                                                            aria-hidden="true">»</span></a></li>
+                                                                aria-hidden="true">»</span></a></li>
                                             </ul>
                                         </nav>
                                     </div>
                                 </div>
                             </div>
                         </div>
+
+                        <br>
+                        <div class="card shadow">
+                            <div class="card-header py-3">
+                                <p class="text-primary m-0 font-weight-bold">Pending Users</p>
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-6 text-nowrap">
+                                        <div id="dataTable_length" class="dataTables_length" aria-controls="dataTable">
+                                            <label>Show&nbsp;<select
+                                                        class="form-control form-control-sm custom-select custom-select-sm">
+                                                    <option value="5" selected="">5</option>
+                                                    <option value="10">10</option>
+                                                </select>&nbsp;</label></div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="text-md-right dataTables_filter" id="dataTable_filter"><label><input
+                                                        type="search" class="form-control form-control-sm"
+                                                        aria-controls="dataTable" placeholder="Search"></label></div>
+                                    </div>
+                                </div>
+                                <div class="table-responsive table mt-2" id="dataTable" role="grid"
+                                     aria-describedby="dataTable_info">
+                                    <table class="table dataTable my-0" id="dataTable">
+                                        <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Username</th>
+                                            <th>Email</th>
+                                            <th>Approve</th>
+                                            <th>Delete</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+
+                                        <?php $sql = "SELECT users.* FROM users WHERE `status`=0";
+                                        $query = $dbh->prepare($sql);
+                                        $query->execute();
+                                        $results = $query->fetchAll(PDO::FETCH_OBJ);
+                                        $cnt = 1;
+                                        if ($query->rowCount() > 0) {
+                                            foreach ($results as $result) {
+                                                $name = htmlentities($result->fname . " " . $result->lname); ?>
+                                                <tr>
+                                                    <td><?php echo htmlentities($cnt); ?></td>
+                                                    <td><?php echo htmlentities($name); ?></td>
+                                                    <td><?php echo htmlentities($result->email); ?></td>
+                                                    <td><a href="manage-users.php?uaid=<?php echo $result->id; ?>"
+                                                           onclick="return confirm('Do you want to approve this user?');">approve</a>
+                                                    </td>
+                                                    <td><a href="manage-users.php?del=<?php echo $result->id; ?>"
+                                                           onclick="return confirm('Do you want to delete?');">delete</a>
+                                                    </td>
+                                                </tr>
+                                                <?php $cnt = $cnt + 1;
+                                            }
+                                        } ?>
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6 align-self-center">
+                                        <p id="dataTable_info" class="dataTables_info" role="status" aria-live="polite">
+                                            Showing 1 to 5 of 100</p>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <nav class="d-lg-flex justify-content-lg-end dataTables_paginate paging_simple_numbers">
+                                            <ul class="pagination">
+                                                <li class="page-item disabled"><a class="page-link" href="#"
+                                                                                  aria-label="Previous"><span
+                                                                aria-hidden="true">«</span></a></li>
+                                                <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                                <li class="page-item"><a class="page-link" href="#"
+                                                                         aria-label="Next"><span
+                                                                aria-hidden="true">»</span></a></li>
+                                            </ul>
+                                        </nav>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
                     </div>
                 </div>
 
