003d0dd057d0d28fd055903dd97428349f50b9f3
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-11-15 12:38:33 +0600

    updates

--- adminv2/index.php
+++ adminv2/index.php
@@ -678,13 +678,7 @@
             <!-- End of Main Content -->
 
             <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2019</span>
-                    </div>
-                </div>
-            </footer>
+            <?php include "includes/footer.php"; ?>
             <!-- End of Footer -->
 
         </div>
