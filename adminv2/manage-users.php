217d3e8db8072495aed768891905347d1b0cb637
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-11-15 14:48:40 +0600

    Update manage-users.php

--- adminv2/manage-users.php
+++ adminv2/manage-users.php
@@ -35,7 +35,7 @@ if (strlen($_SESSION['alogin']) == 0) {
         <meta name="description" content="">
         <meta name="author" content="">
 
-        <title>Manage Posts</title>
+        <title>Manage Users</title>
 
         <!-- Custom fonts for this template-->
         <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
