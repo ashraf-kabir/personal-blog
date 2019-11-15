d33bc8cac156567d1f46e761adc05d6891e3d2fc
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-11-15 20:12:37 +0600

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -117,7 +117,35 @@ if (strlen($_SESSION['alogin']) == 0) {
                                 </div>
                             </div>
 
-                            <!-- Earnings (Monthly) Card Example -->
+                            <!-- Active Posts -->
+                            <div class="col-xl-3 col-md-6 mb-4">
+                                <div class="card border-left-success shadow h-100 py-2">
+                                    <a href="manage-users.php">
+                                        <div class="card-body">
+                                            <div class="row no-gutters align-items-center">
+                                                <div class="col mr-2">
+                                                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                                        Active Posts
+                                                    </div>
+                                                    <?php
+                                                    $sql = "SELECT `id` FROM `posts` WHERE `status`=1";
+                                                    $query = $dbh->prepare($sql);
+                                                    $query->execute();
+                                                    $results = $query->fetchAll(PDO::FETCH_OBJ);
+                                                    $activeposts = $query->rowCount();
+                                                    ?>
+                                                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo htmlentities($activeposts); ?></div>
+                                                </div>
+                                                <div class="col-auto">
+                                                    <i class="fas fa-blog fa-2x text-gray-300"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+
+                            <!-- Pending Posts -->
                             <div class="col-xl-3 col-md-6 mb-4">
                                 <div class="card border-left-primary shadow h-100 py-2">
                                     <a href="manage-posts.php">
@@ -142,37 +170,6 @@ if (strlen($_SESSION['alogin']) == 0) {
                                             </div>
                                         </div>
                                     </a>
-                                </div>
-                            </div>
-
-                            <!-- Earnings (Monthly) Card Example -->
-                            <div class="col-xl-3 col-md-6 mb-4">
-                                <div class="card border-left-info shadow h-100 py-2">
-                                    <div class="card-body">
-                                        <div class="row no-gutters align-items-center">
-                                            <div class="col mr-2">
-                                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                                                    Tasks
-                                                </div>
-                                                <div class="row no-gutters align-items-center">
-                                                    <div class="col-auto">
-                                                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%
-                                                        </div>
-                                                    </div>
-                                                    <div class="col">
-                                                        <div class="progress progress-sm mr-2">
-                                                            <div class="progress-bar bg-info" role="progressbar"
-                                                                 style="width: 50%" aria-valuenow="50" aria-valuemin="0"
-                                                                 aria-valuemax="100"></div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="col-auto">
-                                                <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                                            </div>
-                                        </div>
-                                    </div>
                                 </div>
                             </div>
 
