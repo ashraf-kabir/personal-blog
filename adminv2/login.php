eedb62412c7ce506a932da3332422ab5111bbeb7
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-11-15 14:39:47 +0600

    Update login.php

--- adminv2/login.php
+++ adminv2/login.php
@@ -64,15 +64,15 @@ if (!empty($_SESSION['alogin'])) {
                                         <div class="text-center">
                                             <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                         </div>
-                                        <form class="user">
+                                        <form class="user" method="post">
                                             <div class="form-group">
                                                 <input type="email" class="form-control form-control-user"
                                                        id="exampleInputEmail" aria-describedby="emailHelp"
-                                                       placeholder="Enter Email Address...">
+                                                       placeholder="Enter Email Address..." name="email">
                                             </div>
                                             <div class="form-group">
                                                 <input type="password" class="form-control form-control-user"
-                                                       id="exampleInputPassword" placeholder="Password">
+                                                       id="exampleInputPassword" placeholder="Password" name="password">
                                             </div>
                                             <div class="form-group">
                                                 <div class="custom-control custom-checkbox small">
@@ -82,7 +82,7 @@ if (!empty($_SESSION['alogin'])) {
                                                                                                           Me</label>
                                                 </div>
                                             </div>
-                                            <a href="index.php" class="btn btn-primary btn-user btn-block">
+                                            <a href="index.php" class="btn btn-primary btn-user btn-block" name="login">
                                                 Login
                                             </a>
                                             <hr>
