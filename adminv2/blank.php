d1d856ee79fe8c17fc64aea0667918419f8fc1cd
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-11-15 14:26:58 +0600

    Update blank.php

--- adminv2/blank.php
+++ adminv2/blank.php
@@ -38,7 +38,7 @@ if (isset($_POST['submit'])) {
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Blank</title>
+    <title>Add Post</title>
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
