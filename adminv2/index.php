1eba28082e8c62bcb8b53473552977b33e56af5f
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-11-15 15:21:21 +0600

    Update index.php

--- adminv2/index.php
+++ adminv2/index.php
@@ -404,7 +404,7 @@ if (strlen($_SESSION['alogin']) == 0) {
                     <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                     <div class="modal-footer">
                         <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                        <a class="btn btn-primary" href="login.php">Logout</a>
+                        <a class="btn btn-primary" href="logout.php">Logout</a>
                     </div>
                 </div>
             </div>
