31fabf7034b2ddbcf7627c052cbcf2efb519b30e
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-11-15 19:39:19 +0600

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -17,7 +17,7 @@ if (strlen($_SESSION['alogin']) == 0) {
         <meta name="description" content="">
         <meta name="author" content="">
 
-        <title>SB Admin 2 - Dashboard</title>
+        <title>Admin - Dashboard</title>
 
         <!-- Custom fonts for this template-->
         <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
