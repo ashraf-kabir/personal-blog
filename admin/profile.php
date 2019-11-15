ceaf0f0e76c5e6e19cc6019fadbe7e537028681e
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-11-15 15:42:42 +0600

    Update profile.php

--- admin/profile.php
+++ admin/profile.php
@@ -17,7 +17,7 @@ if (strlen($_SESSION['alogin']) == 0) {
         <meta name="description" content="">
         <meta name="author" content="">
 
-        <title>Add Post</title>
+        <title>Edit Profile</title>
 
         <!-- Custom fonts for this template-->
         <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
