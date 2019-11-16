be9193792a0c49e6b1006034fe77d4f9af709aa5
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-11-16 17:45:11 +0600

    updates

--- register.php
+++ register.php
@@ -210,7 +210,7 @@ if (!empty($_SESSION['login'])) {
                 <div class="card-body p-0">
                     <!-- Nested Row within Card Body -->
                     <div class="row">
-                        <div class="col-lg-5 d-none d-lg-block" style="background-image: url(admin/img/bugsbunny01.jpg);"></div>
+                        <div class="col-lg-5 d-none d-lg-block" style="background-image: url(admin/img/regimage.jpg);"></div>
                         <div class="col-lg-7">
                             <div class="p-5">
                                 <div class="text-center">
