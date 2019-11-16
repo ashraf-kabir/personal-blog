73c9bba5579a8774af9241a7107def16e1147bcc
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-11-16 17:37:34 +0600

    changes

--- admin/login.php
+++ admin/login.php
@@ -94,11 +94,11 @@ if (!empty($_SESSION['alogin'])) {
                         <div class="card-body p-0">
                             <!-- Nested Row within Card Body -->
                             <div class="row">
-                                <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                                <div class="col-lg-6 d-none d-lg-block" style="background-image: url(img/blue.jpg);"></div>
                                 <div class="col-lg-6">
                                     <div class="p-5">
                                         <div class="text-center">
-                                            <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                            <h1 class="h4 text-gray-900 mb-4">Admin Login</h1>
                                         </div>
                                         <form class="user" method="post" name="userlogin" onsubmit="return validate();" novalidate>
                                             <div class="form-group">
