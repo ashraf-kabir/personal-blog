42a33db9977439d6ae463d8db2a6cce6fc34e778
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-11-16 20:01:54 +0600

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,14 @@
                 <li class="nav-item" role="presentation"><a class="nav-link" href="view-posts.php">View Posts</a></li>
                 <?php if (strlen($_SESSION['login']) != 0) {
                     ?>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="manage-posts.php">Manage Posts</a></li>
+                    <li class="nav-item dropdown" role="presentation"><a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Manage Posts
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink">
+                            <a class="dropdown-item" href="add-post.php">Add Post</a>
+                            <a class="dropdown-item" href="manage-posts.php">Edit Post</a>
+                        </div>
+                    </li>
                 <?php } else { }?>
                 <?php if (strlen($_SESSION['login']) == 0) {
                     ?>
@@ -27,7 +34,17 @@
                     if ($query->rowCount() > 0) {
                         foreach ($results as $result2) {
                             ?>
-                            <li class="nav-item" role="presentation"><a class="nav-link" href="profile.php"><?php echo htmlentities($result2->fname." ".$result2->lname); ?></a></li>
+                            <li class="nav-item dropdown" role="presentation">
+                                <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <?php echo htmlentities($result2->fname." ".$result2->lname); ?>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink">
+                                    <a class="dropdown-item" href="profile.php">Edit Profile</a>
+                                    <a class="dropdown-item" href="update-password.php">Update Password</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="logout.php">Log out</a>
+                                </div>
+                            </li>
                         <?php }
                     }
                 } ?>
@@ -35,11 +52,7 @@
                 ?>
                 <li class="nav-item" role="presentation"><a class="nav-link" href="register.php">Register</a></li>
                 <?php } else { }?>
-                <?php if (strlen($_SESSION['login']) == 0) {
-                    ?>
-                <?php } else { ?>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="logout.php">Log out</a></li>
-                <?php } ?>
+
             </ul>
         </div>
     </div>
