1aa766e60137b72fe49d0fd3f82cf98286c91a58
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-11-16 08:54:31 +0600

    Update login.php

--- login.php
+++ login.php
@@ -102,7 +102,7 @@ if (!empty($_SESSION['login'])) {
                                 <div class="col-lg-6">
                                     <div class="p-5">
                                         <div class="text-center">
-                                            <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                            <h1 class="h4 text-gray-900 mb-4">User Login</h1>
                                         </div>
                                         <form class="user" method="post" id="userform" name="userlogin"
                                               onsubmit="return validate();" novalidate>
