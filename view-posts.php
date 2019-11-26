e61aca3cad1891268920daca3c39514820e89447
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-11-27 02:24:51 +0600

    Update view-posts.php

--- view-posts.php
+++ view-posts.php
@@ -11,7 +11,7 @@ $_SESSION['redirectURL'] = $_SERVER['REQUEST_URI'];
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Blog Post</title>
+    <title>Blog Posts</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
